2870a940265579a7bcc45b06048afd335c3b1013
Gabe Pike <gpike@isecpartners.com>
2013-04-28 00:43:29 -0700

    fixing p11 kind of

--- p11.py
+++ p11.py
@@ -3,23 +3,29 @@ from Crypto.Cipher import AES
 from random import randint
 from p7 import aes_ecb_encrypt, aes_ecb_decrypt
 from p10 import aes_cbc_encrypt, aes_cbc_decrypt
+from p8 import chunks, detect_ecb
 import sys
 
-def aes_rand_encrypt(pt):
-  key = Random.new().read(AES.block_size)
+def encryption_oracle(pt): 
+  entropy = Random.new()
+  key = entropy.read(AES.block_size)
   crypt = randint(0,1)
-  iv = b"\x00"*AES.block_size
-  rand_prefix = Random.new().read(randint(5, 10))
-  rand_suffix = Random.new().read(randint(5, 10))
-  pt = rand_prefix + pt + rand_suffix
+
+  prefix = entropy.read(randint(5,10))
+  suffix = entropy.read(randint(5,10))
+
+  pt = prefix + pt + suffix
 
   if crypt == 0:
+    print("Crypting in ECB mode")
     return aes_ecb_encrypt(pt, key)
   elif crypt == 1:
+    print("Crypting in CBC mode")
+    iv = entropy.read(AES.block_size)
     return aes_cbc_encrypt(pt, key, iv)
 
 def main():
-  print(aes_rand_encrypt(b'A'*80))
-
+  print("ECB" if detect_ecb(encryption_oracle(b'A'*512)) else "CBC")
+    
 if __name__ == '__main__':
   sys.exit(main())
