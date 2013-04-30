e3d37fe71924fe412d86f724c70e7f28557fa46a
Gabe Pike <gpike@isecpartners.com>
2013-04-30 16:56:41 -0700

    got p14 working - needs cleanup

--- p14.py
+++ p14.py
@@ -18,22 +18,24 @@ Did you stop? No, I just drove by
 
 from Crypto import Random
 from Crypto.Cipher import AES
+from random import randint
 from base64 import b64decode
 from helpers import chunks
 from cryptlib import aes_ecb_encrypt, aes_ecb_decrypt
 import sys
+from time import sleep
 
 key = Random.new().read(AES.block_size)
 random_prefix = Random.new().read(randint(0, 256))
 unknown_string = b64decode(b"Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK")
 
 def encryption_oracle(pt): 
-  return aes_ecb_encrypt(pt+unknown_string, key)
+  return aes_ecb_encrypt(random_prefix+pt+unknown_string, key)
 
-def find_blocksize(encrypt_func):
-  ct_len = len(encrypt_func(b'A'*1))
+def find_blocksize(cryptf):
+  ct_len = len(cryptf(b'A'*1))
   for i in range(2, 512+1):
-    _ = len(encrypt_func(b'A'*i))
+    _ = len(cryptf(b'A'*i))
     if (_ > ct_len):
       return _ - ct_len
 
@@ -49,30 +51,63 @@ def detect_ecb(cryptf, block_size):
   ct1 = chunks(cryptf(pt1), block_size)
   ct2 = chunks(cryptf(pt2), block_size)
 
-  return ct1[1] == ct2[1]
+  return ct1[-1] == ct2[-1]
 
-def decrypt_ecb():
-  block_size = find_blocksize(encryption_oracle)
-  if not detect_ecb(encryption_oracle, block_size):
+def find_plaintext_alignment(cryptf):
+  ct1 = cryptf(b'A')
+  ct2 = cryptf(b'B')
+  bsz = find_blocksize(cryptf)
+
+  blockno = -1
+  for i, (blk1, blk2) in enumerate(zip(chunks(ct1, bsz), chunks(ct2, bsz))):
+    if blk1 != blk2:
+      blockno = i
+      break
+  else:
+    raise Exception("Could not find delta block!")
+
+  nextblockno = blockno + 1
+  nextblock = chunks(ct1, bsz)[nextblockno]
+
+  offset = -1
+  for i in range(bsz+1):
+    nextblock1 = chunks(cryptf(b'A'*i+b'A'), bsz)[nextblockno]
+    nextblock2 = chunks(cryptf(b'A'*i+b'B'), bsz)[nextblockno]
+    if nextblock1 != nextblock2:
+      offset = i
+      break
+  else:
+    raise Exception("Could not find alignment offset!")
+
+  return nextblockno, offset
+
+def decrypt_ecb(blockno, offset):
+  blksz = find_blocksize(encryption_oracle)
+  blockpos = blockno*blksz
+  if not detect_ecb(encryption_oracle, blksz):
     raise Exception("Cipher not in ECB mode")
 
   ciphertext = encryption_oracle(b'')
-  plaintext = b'A'*block_size
+  plaintext = b'B'*blksz
   pt_block = b''
-  i = 1
-  for i in range(0, len(ciphertext)+1):
+  for i in range(blockpos+1, len(ciphertext)+offset):
 # calculate known plaintext bytes used to pad
-    dummy = plaintext[-(block_size-i%16):] if i % 16 != 0 else b''
-# get position of ciphertext block we want to break
-    pos = (len(plaintext) // block_size - 1)*block_size
+    dummy = b'A'*offset
+
+    targetpt = dummy+plaintext[-(blksz-i%16):] if i % 16 != 0 else dummy
 # ciphertext we are trying to match with our known plaintext 
-    target = encryption_oracle(dummy)[pos:pos+block_size]
+    targetct = encryption_oracle(targetpt)
+# get position of ciphertext block we want to break
+    targetno = (len(plaintext) // blksz - 1) + blockno
+    targetblk = chunks(targetct, blksz)[targetno]
+
     for j in range(0,255+1):
-      guess = dummy+pt_block+bytes([j])
-      ct = encryption_oracle(guess)[:block_size]
-      if ct == target:
+      guesspt = targetpt+pt_block+bytes([j])
+      guessct = encryption_oracle(guesspt)
+      guessblk = chunks(guessct, blksz)[blockno]
+      if guessblk == targetblk:
         pt_block += bytes([j])
-        if len(pt_block) == block_size:
+        if len(pt_block) == blksz:
           plaintext += pt_block
           pt_block = b''
         break
@@ -81,35 +116,16 @@ def decrypt_ecb():
   if pt_block:
     plaintext += pt_block
 
-  """ 
-  If using pkcs7 padding, the last byte will always be 0x01 because the
-  program assumes 0x01 is a valid byte, even though it is padding, and no
-  other ciphertexts after that will be able to match the actual padding. 
-  """ 
+  return plaintext[blksz:-1]
 
-  return plaintext[block_size:-1]
+
+def decrypt_ecb_randprefix():
+  blockpos, offset = find_plaintext_alignment(encryption_oracle)
+  return decrypt_ecb(blockpos, offset)
+
 
 def main():
   """
-  process:
-  1. detect block boundary and block size
-    * to make things cleaner, initialize decrypted bytes to a block of padding bytes
-  2. decrypt last block:
-    a. shift plaintext so that its last byte is the first byte of the last block
-    b. you know what the padding will be. brute-force the first byte
-    c. brute-force the next bytes and so on until all the block is fully recovered
-    d. prepend decrypted block to result bytestring
-  3. decrypt the rest of the blocks:
-    a. start by shifting the last block_size-1 decrypted bytes into the last block_size-1 bytes of the
-    b. brute-force the first byte (last block_size-1 bytes are known)
-    c. brute force the next bytes and so on until all the block is fully recovered
-    d. prepend each decrypted block to result bytestring
-  4. decrypt the last block
-    a. last block will likely contain dummy characters
-    b. fill block so that it only contains known dummy characters and target string
-    c. brute-force starting from first unknown character
-    d. discard dummy characters when prepending last block to result bytestring
-
   BETTER PROCESS:
   Find out where the plaintext we want begins
   1. call oracle function with incremental dummy text until block boundary is found
@@ -122,7 +138,7 @@ def main():
 
   """
 
-  print(decrypt_ecb().decode('utf8'))
+  print(decrypt_ecb_randprefix().decode('utf8'))
     
 if __name__ == '__main__':
   sys.exit(main())
