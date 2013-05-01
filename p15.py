14bfea175ce595182e4f1ebc0a4ab8bd8e40dc78
Gabe Pike <gpike@isecpartners.com>
2013-04-30 21:59:27 -0700

    add problem 15

---
+++ p15.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.2
+
+_author_ = "Gabe Pike"
+_email_ = "gpike@isecpartners.com"
+
+from mycrypto import pkcs7_pad, pkcs7_unpad
+import sys
+
+def main():
+  block_size = 16
+  try:
+    print(pkcs7_unpad(b"ICE ICE BABY\x04\x04\x04\x04", block_size))
+    print(pkcs7_unpad(b"ICE ICE BABY\x01\x02\x03\x04", block_size))
+  except Exception as e:
+    print(e)
+
+if __name__ == '__main__':
+  sys.exit(main())
