41a4501d7e74b5827cf035417a6a9705457bb291
Gabe Pike <gpike@isecpartners.com>
2013-06-21 21:30:48 -0700

    got hle attack working; need to guess keylen now

--- p29.py
+++ p29.py
@@ -1,16 +1,13 @@
 import struct
 from sys import exit
-from slowsha import sha1, SHA1
+from thirdparty.sha1 import sha1
+from base64 import b16encode
 
 def MAC(message, key):
-    return sha1(key + message).digest()
+    return sha1(key + message)
 
 def authenticate(message, key, mac):
     return MAC(message, key) == mac
-
-def validate_message(message, key, mac):
-    print('message: %s is %s' % (message, 
-          "valid" if authenticate(message,key,mac) else "invalid"))
 
 def sha1_pad(msg, prefix_len=0, key_len=0):
     
@@ -24,16 +21,13 @@ def sha1_pad(msg, prefix_len=0, key_len=0):
     return msg + one_pad + zero_pad + len_pad
 
 def hash_length_extension(prefix, mac, injection, key_len):
-    print(mac)
 
     prefix_len = 64 * ((len(prefix) + key_len) // 64) + 64
     padded_prefix = sha1_pad(prefix, key_len=key_len)
     padded_injection = sha1_pad(injection, prefix_len=prefix_len)
 
     forged_message = padded_prefix + injection
-    forged_mac = SHA1(message=padded_injection, seed=mac).digest()
-    print(padded_injection)
-    print(forged_mac)
+    forged_mac = sha1(message=padded_injection, seed=mac)
     return forged_message, forged_mac
 
 def main():
@@ -45,9 +39,23 @@ def main():
     mac = MAC(message, key)
     injection = b'&to=666&amount=99999'
 
+    print("Key:     %s" % key)
+    print()
+    print("Message: %s " % str(message, 'utf8'))
+    print("MAC:     %s" % str(b16encode(mac).lower(), 'utf8'))
+    print()
+
     forged_message, forged_mac = (
             hash_length_extension(message, mac, injection, len(key)))
-    validate_message(forged_message, key, forged_mac)
+
+    print("Forged message: %s " % forged_message)
+    print("Forged MAC:     %s " % str(b16encode(forged_mac).lower(), 'utf8'))
+    print()
+
+    if authenticate(forged_message, key, forged_mac):
+        print("Authenticated the message")
+    else:
+        print("Failed to authenticate the message")
 
 if __name__ == '__main__':
     exit(main())
