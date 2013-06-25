9f299b68c603eab911a60a9da118f57225bb010f
Gabe Pike <gpike@isecpartners.com>
2013-06-24 22:52:36 -0700

    p28: use new sha1 lib

--- p28.py
+++ p28.py
@@ -1,13 +1,14 @@
 from sys import exit
-from slowsha import sha1
+from thirdparty.sha1 import sha1
 
 def MAC(message, key):
-    return sha1(key + message).digest()
+    return sha1(key + message)
 
 def authenticate(message, key, mac):
     return MAC(message, key) == mac
 
 def validate_message(message, key, mac):
+    print("\nkey:     %s" % str(key, 'utf8'))
     print('message: "%s" (%s)' % (message.decode('utf8'), 
           "valid" if authenticate(message,key,mac) else "invalid"))
 
@@ -15,6 +16,8 @@ def main():
     key = b'123456'
     message = b'no one can break this crypto because i am an OG'
     mac = MAC(message, key)
+    print("original message: %s" % str(message, 'utf8'))
+    print("original key:     %s" % str(key, 'utf8'))
 
     validate_message(message, key, mac)
     validate_message(message[:-1]+b'J', key, mac)
