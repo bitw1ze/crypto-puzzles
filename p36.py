912b53e7943e3123f94f046b1d9d4f665e03131f
Gabe Pike <gpike@isecpartners.com>
2013-07-24 02:00:37 -0700

    code cleanup

--- p36.py
+++ p36.py
@@ -9,11 +9,8 @@ from base64 import b16encode
 N = 0xffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff
 g = 2
 k = 3
-identity=b'admin'
+username=b'admin'
 password=b'muhpassword'
-
-client = {}
-server = {}
 
 # server generates v from the password verifier and DH group params
 salt = os.urandom(8)
@@ -28,7 +25,6 @@ A = pow(g, a, N)
 # server sends B to client
 b = int(str(b16encode(os.urandom(72)), 'utf8'), 16)
 B = k*v + pow(g, b, N)
-# sock.send(salt + B)
 
 # client and server calculate 'u' from the public keys
 uH = sha256(bytes(str(A) + str(B), 'utf8')).hexdigest()
