9b61b89637e056a72140e5cb3837beb66cfcc91f
Gabe Pike <gpike@isecpartners.com>
2013-08-17 23:23:44 -0700

    added conversion functions to helpers

--- helpers.py
+++ helpers.py
@@ -9,6 +9,7 @@ the challenges.
 """
 
 from functools import reduce
+from base64 import b16encode, b16decode
 
 def chunks(s, n):
   return [bytes(s[i:i+n]) for i in range(0, len(s), n)]
@@ -18,3 +19,33 @@ def flatten(lst):
 
 def identity(*args):
   return args[0]
+
+def b2i(data):
+
+    _ = str(b16encode(data), 'utf8')
+    if len(_) % 2 is not 0:
+        _ = '0' + _
+    return int(_, 16)
+
+def i2b(data):
+
+    x = hex(data)[2:]
+    if len(x) % 2 != 0:
+        x = '0' + x
+    x = bytes(x, 'utf8')
+    return b16decode(x, True)
+
+def b2u(data):
+
+    return str(data, 'utf8')
+
+def u2b(data):
+
+    return bytes(data, 'utf8')
+
+def b2hs(data):
+
+    x = b2u(b16encode(data))
+    if len(x) % 2 != 0:
+        x = '0' + x
+    return x
