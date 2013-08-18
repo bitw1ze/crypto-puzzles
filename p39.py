ea51565aa86c90ec99abb38bc0a2e408859f86fa
Gabe Pike <gpike@isecpartners.com>
2013-08-18 12:06:27 -0700

    touchups on p39 and p40

--- p39.py
+++ p39.py
@@ -1,76 +1,27 @@
 #!/usr/bin/env python3.2
 
-from base64 import b16encode, b16decode
-from sys import exit
+import sys
 
-# yes, i am this lazy
-from fractions import gcd
-from Crypto.PublicKey.pubkey import getStrongPrime
-
-from helpers import *
-
-def egcd(a, b):
-    
-    if a == 0:
-        return (b, 0, 1)
-    else:
-        g, y, x = egcd(b % a, a)
-        return (g, x - (b // a) * y, y)
-    
-def invmod(a, m):
-
-    g, x, y = egcd(a, m)
-    return x % m if g == 1 else 0
-
-def rsa_encrypt_bytes(pubkey, message):
-
-    return _rsa_crypt_bytes(pubkey, message)
-
-def rsa_decrypt_bytes(privkey, message):
-
-    return _rsa_crypt_bytes(privkey, message)
-
-def _rsa_crypt_bytes(k, msg):
-
-    k = (b2i(k[0]), b2i(k[1]))
-    return i2b(_rsa_crypt(k, b2i(msg)))
-
-def _rsa_crypt(k, msg):
-
-    return pow(msg, k[0], k[1])
-
-def generate_keypair_bytes(bits):
-
-    pubkey, privkey = _generate_keypair(bits)
-    return (i2b(pubkey[0]), i2b(pubkey[1])), (i2b(privkey[0]), i2b(privkey[1]))
-
-def _generate_keypair(bits):
-
-    e = 3
-    while True:
-
-        p = getStrongPrime(bits)
-        q = getStrongPrime(bits)
-        n = p * q
-        et = (p-1)*(q-1)
-
-        if gcd(et, e) == 1:
-            break
-    d = invmod(e, et)
-    return ((e, n), (d, n))
+from helpers import b2hs, b2u
+from myrsa import rsa_encrypt_bytes, rsa_decrypt_bytes, generate_keypair_bytes
 
 def main():
 
-    pubkey, privkey = generate_keypair_bytes(1024)
+    print("Let's encrypt a message!")
+    print("Generating a 1024 bit public key...")
+    KEY_SIZE = 1024
+    pubkey, privkey = generate_keypair_bytes(KEY_SIZE)
     print("e:", b2hs(pubkey[0]))
     print("d:", b2hs(privkey[0]))
     print("n:", b2hs(privkey[1]))
     message = b"I be tossin', enforcin', my style is awesome / I'm causin' more Family Feuds than Richard Dawson!"
     ciphertext = rsa_encrypt_bytes(pubkey, message)
     plaintext = rsa_decrypt_bytes(privkey, ciphertext)
-    print("encrypted:", b2hs(ciphertext))
-    print("decrypted:", b2u(plaintext))
+    print("=================== encrypted message ===================")
+    print(b2hs(ciphertext))
+    print("=================== decrypted message ===================")
+    print(b2u(plaintext))
 
 if __name__ == '__main__':
 
-    exit(main())
+    sys.exit(main())
--- p40.py
+++ p40.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python3.2
 
-from base64 import b16encode, b16decode
 from functools import reduce
 import operator
+import sys
 
-from helpers import *
+from helpers import b2i, i2s
 from myrsa import generate_keypair_bytes, rsa_encrypt_bytes
 from mymath import invmod, root3
 
 def main():
 
     KEY_SIZE = 512
-    msg = b"SUPER SECRET"
+    msg = b"SUPER FSCKING SECRET SAUCE"
     pubkeys = [generate_keypair_bytes(KEY_SIZE)[0] for i in range(3)]
     cts = [b2i(rsa_encrypt_bytes(k, msg)) for k in pubkeys]
     moduli = [b2i(i[1]) for i in pubkeys]
     crt = 0
 
+    # calculate T_0, T_1, T_2
     for i in range(3):
         m_s = reduce(operator.mul, [moduli[j] if i != j else 1 for j in range(3)], 1)
         crt += (cts[i] * m_s * invmod(m_s, moduli[i]))
 
-    n012 = reduce(operator.mul, [m for m in moduli], 1)
-    crt = crt % n012
+    # mod the result by n_0*n_1*n_2 then calc cube root it
+    n_012 = reduce(operator.mul, [m for m in moduli], 1)
+    crt = crt % n_012
     plaintext = root3(crt)
-    print(i2s(plaintext))
+    print("Decrypted the message!:", i2s(plaintext))
 
 if __name__ == '__main__':
-
-    exit(main())
+    sys.exit(main())
